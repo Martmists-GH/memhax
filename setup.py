a9b316455ef84efa450b0e32fea2cebefb2f4a4b
Martmists <martmists@gmail.com>
2022-09-10 02:07:35 +0200

    fix setup.py
    Signed-off-by: Martmists <martmists@gmail.com>

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ except FileNotFoundError:
 setup(
     name="memhax",
     version="0.1.0",
-    packages=['memhax', 'memhax.cpython'],
+    packages=['memhax', 'memhax.cpython', 'memhax.native'],
     description="A Python library for getting access to raw memory and internals",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.11,<3.12',  # TODO: Support other python versions
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: Unix",
         "Intended Audience :: Developers",
